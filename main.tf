216e94e0fbb4524b14664bc587907eac8504bcc6
Xmen143 <83784820+Xmen143@users.noreply.github.com>
2023-09-15 10:36:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "example" {
+  triggers = {
+    # Change this trigger whenever you want to re-run the command
+    always_run = "${timestamp()}"
+  }
+
+ 
+
+  # Execute a shell command when this resource is created or updated
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!'"
+  }
+}
+
+ 
+
+# Output the result of the shell command
+output "shell_output" {
+  value = null_resource.example.triggers.always_run
+}
